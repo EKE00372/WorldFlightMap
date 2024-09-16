57a32816e1cb59c0230d61dd80e60c1d67429544
ZYX <eke00372@gmail.com>
2024-09-16 13:43:22 +0800

    Kanegasi:  fix LinePoolMixin

--- LinePool.lua
+++ LinePool.lua
@@ -1,29 +1,23 @@
 ---- LINE POOL ----
 -- copy/pasta of TexturePoolMixin, just for lines
+local LinePoolMixin = {}
 
-local LinePoolMixin = CreateFromMixins(ObjectPoolMixin)
-local function LinePoolFactory(linePool)
-	return linePool.parent:CreateLine(nil, linePool.layer, linePool.textureTemplate, linePool.subLayer)
-end
-
-function LinePoolMixin:OnLoad(parent, layer, subLayer, textureTemplate, resetterFunc)
-	ObjectPoolMixin.OnLoad(self, LinePoolFactory, resetterFunc)
-	self.parent = parent
-	self.layer = layer
-	self.subLayer = subLayer
-	self.textureTemplate = textureTemplate
-end
-
---[[ global ]]
 local function LinePoolFactory(linePool)
     return linePool.parent:CreateLine(nil, linePool.layer, linePool.textureTemplate, linePool.subLayer)
 end
 
+function LinePoolMixin:OnLoad(parent, layer, subLayer, textureTemplate, resetterFunc)
+    Mixin(self, CreateUnsecuredObjectPool(LinePoolFactory, resetterFunc))
+    Mixin(self, CreateUnsecuredTexturePool(parent, layer, subLayer, textureTemplate, resetterFunc))
+    self:Init(LinePoolFactory, resetterFunc)
+    self.parent = parent
+    self.layer = layer
+    self.subLayer = subLayer
+    self.textureTemplate = textureTemplate
+end
+
 function CreateLinePool(parent, layer, subLayer, textureTemplate, resetterFunc)
-    local linePool = CreateSecureObjectPool(LinePoolFactory)
-    linePool.parent = parent
-    linePool.layer = layer
-    linePool.subLayer = subLayer
-    linePool.textureTemplate = textureTemplate
+    local linePool = CreateFromMixins(LinePoolMixin)
+    linePool:OnLoad(parent, layer, subLayer, textureTemplate, resetterFunc or Pool_HideAndClearAnchors)
     return linePool
 end
